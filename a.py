45d6a719c7314f4849cad8b0f189d6ed7a434da0
chakma21 <shinychakma2003@gmail.com>
2025-07-25 22:30:47 +0530

    1b complete

--- a.py
+++
@@ -1,4 +0,0 @@
-from sentence_transformers import SentenceTransformer
-model = SentenceTransformer('all-MiniLM-L6-v2')
-model.save_pretrained('C:/Users/tokos/Desktop/project/adobe/models/all-MiniLM-L6-v2')
-print("Model downloaded and saved.")
